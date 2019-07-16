73eb06a43c5eed3c77dfad8f3b65b777ea393a56
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-07-16 20:30:36 -0300

    exibindo msg de erro para usuario

--- js/importar.js
+++ js/importar.js
@@ -1,18 +1,24 @@
 var botaoImportar = document.querySelector("#buscar-paciente");
-botaoImportar.addEventListener("click", function(event) {
+botaoImportar.addEventListener("click", function (event) {
     event.preventDefault();
 
     console.log("Buscando pacientes...");
 
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-    xhr.addEventListener("load", function(){
-        var pacientes = JSON.parse(xhr.responseText);
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes2");
+    xhr.addEventListener("load", function () {
 
-        pacientes.forEach(function(paciente){
-            adicionaPacienteNaTabela(paciente);
-        });
+        if (xhr.status == 200) {
+            var pacientes = JSON.parse(xhr.responseText);
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log("Error: " + xhr.status);
+            var errorAjax = document.querySelector("#error-ajax");
+            errorAjax.classList.remove("invisivel");            
+        }
     });
     xhr.send();
 });
